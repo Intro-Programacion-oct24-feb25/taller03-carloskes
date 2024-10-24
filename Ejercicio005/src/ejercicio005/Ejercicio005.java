bbdc8e3177e87dfaf13573f99b492266c403681d
carloskes <cscorrea@utpl.edu.ec>
2024-10-24 12:01:47 -0500

    actualizado

---
+++ Ejercicio005/src/ejercicio005/Ejercicio005.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio005;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio005 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double valor1 = (Math.pow(2, 2));
+        System.out.println((valor1 + 12 -1)/3);
+    }
+    
+}
