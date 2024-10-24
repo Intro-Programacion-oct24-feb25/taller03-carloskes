c7dae61334e0af8dba4eb111ce518b0cdaa84f20
carloskes <cscorrea@utpl.edu.ec>
2024-10-24 12:41:47 -0500

    actualizado

---
+++ Ejercicio008/src/ejercicio008/Ejercicio008.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio008;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio008 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double valor1 = ((10*9) + 20 - (500/2) + Math.pow(3, 2));
+    boolean valor2 = valor1 <= 200;
+   
+    
+   boolean valor3 = 50 <= (50-50+51);
+   
+   boolean valor4 = valor2 || valor3;
+   System.out.println(valor4);
+          
+        // TODO code application logic here
+    }
+    
+}
