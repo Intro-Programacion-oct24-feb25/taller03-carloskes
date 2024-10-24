c5dcb55f3a2bd8d641d06d6be5627aafff7ce127
carloskes <cscorrea@utpl.edu.ec>
2024-10-24 12:45:14 -0500

    actualizado

---
+++ Ejercicio009/src/ejercicio009/Ejercicio009.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio009;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio009 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean valor1 = Math.sqrt(81) + Math.sqrt(25) * 10 >= 50;
+        System.out.println(valor1);
+        
+                
+                
+        // TODO code application logic here
+    }
+    
+}
