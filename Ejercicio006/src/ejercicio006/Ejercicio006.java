b0d83186592ae36efe5b521c63a4327eb04616d4
carloskes <cscorrea@utpl.edu.ec>
2024-10-24 12:20:19 -0500

    actualizado

---
+++ Ejercicio006/src/ejercicio006/Ejercicio006.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio006;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio006 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double valor1 = ((Math.sqrt(81) + 9)/3);
+        boolean valor2 = valor1 == 9;
+        boolean valor3 = valor2 && 10 < 1;
+        System.out.println(valor3);
+        
+        // TODO code application logic here
+    }
+    
+}
