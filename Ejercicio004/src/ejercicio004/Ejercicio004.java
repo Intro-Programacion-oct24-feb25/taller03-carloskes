e975669751596d0b0b5c6d36f426037c52b6648e
carloskes <cscorrea@utpl.edu.ec>
2024-10-24 11:52:02 -0500

    actualizado

---
+++ Ejercicio004/src/ejercicio004/Ejercicio004.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio004;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio004 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean valor1 = 20 / (1 + 3) +2 >= 3;  
+        
+        
+        System.out.println(valor1);
+        
+        
+        
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
